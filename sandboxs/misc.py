229eed62343c06761c1f86838c677192fc7a6de7
dw <dwhan89@gmail.com>
2019-09-06 14:37:55 -0400

    updates

--- sandboxs/misc.py
+++ sandboxs/misc.py
@@ -1,6 +1,8 @@
 ##
 #  miscellenous scripts used throughout the code
 ##
+import os
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -8,3 +10,13 @@ def str2bool(v):
         return False
     else:
         raise TypeError("Can't convert 'str' object to 'boolean'")
+
+def create_dir(path_to_dir):
+    ''' check wether the directory already exists. if not, create it '''
+    if not os.path.isdir(path_to_dir):
+        os.makedirs(path_to_dir)
+        exit = 0
+    else:
+        exit = 1
+
+    return exit

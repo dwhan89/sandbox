b1113943e35f274636a283773078e526d9ded0fb
DW <dwhan89@gmail.com>
2019-10-28 14:26:14 -0400

    plotting code

--- bin/make_tiles.py
+++ bin/make_tiles.py
@@ -1,13 +1,28 @@
-from pixell import enmap
+from pixell import enmap, enplot
 from soapack import interfaces as soint
 import os
 
-patches = soint.models['act_mr3']()
+overwrite = False
+
 output_dir = "/mnt/ceph/users/dhan/images"
 output_path = lambda x: os.path.join(output_dir, x)
 
+DM = soint.models['act_mr3'](calibrated=False)
+patches = DM.get_psa_indexes()
+
 for psa in patches:
     season, patch, array, freq = psa.split('_')
+    array_freq = '{}_{}'.format(array, freq)
+
+    ivar_file = output_path("{}_ivar_gray.png".format(psa))
+    if not os.path.exists(ivar_file) or overwrite:
+        ivar = DM.get_coadd_ivar(season, patch, array_freq)
+        shape, wcs = ivar.shape, ivar.wcs
+
+        eplots = enplot.plot(ivar, color='gray', downgrade=2, grid=True, mask=0)
+        for eplot in eplots:
+            eplot.write(ivar_file)
 
 
-
+    #emap = DM.get_coadd(season, patch, array_freq, srcfree=False, ncomp=None)
+    #eplots =

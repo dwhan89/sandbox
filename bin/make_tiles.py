f5d53cfb382c80afb8962e8fe6090f9658f49876
DW <dwhan89@gmail.com>
2019-10-28 14:03:17 -0400

    updates

--- bin/make_tiles.py
+++ bin/make_tiles.py
@@ -1,1 +1,13 @@
+from pixell import enmap
+from soapack import interfaces as soint
+import os
+
+patches = soint.models['act_mr3']()
+output_dir = "/mnt/ceph/users/dhan/images"
+output_path = lambda x: os.path.join(output_dir, x)
+
+for psa in patches:
+    season, patch, array, freq = psa.split('_')
+
+
 

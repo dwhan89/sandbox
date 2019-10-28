0b0f42f77898deb6c66c76abe7ae0df73d8fc1d2
DW <dwhan89@gmail.com>
2019-10-28 16:00:24 -0400

    updates

--- bin/make_tiles.py
+++ bin/make_tiles.py
@@ -1,4 +1,4 @@
-from pixell import enmap, enplot
+from pixell import enmap, enplot, curvedsky
 from soapack import interfaces as soint
 import os
 from sandbox import misc
@@ -25,10 +25,35 @@ for psa in patches:
 
         eplots = enplot.plot(ivar, color='gray', downgrade=2, grid=True, mask=0)
         for eplot in eplots:
-            eplot.write(ivar_file, eplot)
+            enplot.write(ivar_file, eplot)
 
         del eplots
     del ivar
 
-    #emap = DM.get_coadd(season, patch, array_freq, srcfree=False, ncomp=None)
-    #eplots =
+    misc.create_dir(output_path("data"))
+    cmb_file_temp = output_path("data/%s_{}_data_color.png"%(psa))
+
+    if not os.path.exists(cmb_file_temp.format("U")) or ovewrite:
+        emaps = DM.get_coadd(season, patch, array_freq, srcfree=False, ncomp=None)
+        eplots = enplot.plot(emaps, downgrade=2, grid=True, mask=0)
+
+        for i, cmb_idx in enumerate(['I','Q','U']):
+            enplot.write(cmb_file_temp.format(cmb_idx), eplots[i])
+
+        del eplots, emaps
+
+
+    if not os.path.exists(cmb_file_temp.format("B")) or ovewrite:
+        emaps = DM.get_coadd(season, patch, array_freq, srcfree=False, ncomp=None)
+        sshape, swcs = emaps.shape, emaps.wcs
+
+        lmax = 8000
+        alms = curvedsky.map2alm(emaps, lmax=lmax, spin=[0,2])
+        emaps = curvedsky.alm2map(alms, enmap.zeros(sshape, swcs), spin=[0,2])
+        eplots = enplot.plot(emaps[1:,...], downgrade=2, grid=True)
+
+        for i, cmb_idx in enumerate(['E', 'B']):
+            enplot.write(cmb_file_temp.format(cmb_idx), eplots[i])
+
+        del alms, emaps, eplots
+

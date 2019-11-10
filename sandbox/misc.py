b764e686ba631669ef01ca0769c459cac65737ff
DW <dwhan89@gmail.com>
2019-11-09 20:18:52 -0500

    refactoring

--- sandbox/misc.py
+++ sandbox/misc.py
@@ -3,6 +3,7 @@
 ##
 import os
 
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -10,6 +11,7 @@ def str2bool(v):
         return False
     else:
         raise TypeError("Can't convert 'str' object to 'boolean'")
+
 
 def create_dir(path_to_dir):
     ''' check wether the directory already exists. if not, create it '''
@@ -20,3 +22,13 @@ def create_dir(path_to_dir):
         exit = 1
 
     return exit
+
+
+def progress(count, total, status=''):
+    # adoped from https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+    bar_len = 60
+    percents = 0
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    print('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
